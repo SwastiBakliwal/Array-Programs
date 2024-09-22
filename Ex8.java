f4d1bc699c988b91193718092d7871ad26c9f850
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-09-22 20:42:19 +0530

    Add files via upload

---
+++ Ex8.java
@@ -0,0 +1,54 @@
+class Ex8 
+{
+// Java Program to check if the arrays are equal 
+
+
+	public static void main(String[] args) 
+	{ 
+		// Initializing the first array 
+		int a[] = { 10, 30, 12 }; 
+		// Initializing the second array 
+		int b[] = { 10, 30, 12 }; 
+
+		// store the result 
+		boolean result = true; 
+
+		// Check if length of the two arrays are equal or 
+		// not 
+		if (a.length == b.length) { 
+			
+			// Loop to check elements of arrays one by one 
+			for (int i = 0; i < a.length; i = i + 1) { 
+				
+				// To check if any element is different 
+				if (a[i] != b[i]) { 
+					
+					// If any element is different then it 
+					// will assign false into boolean 
+					// variable 
+					result = false; 
+				} 
+			} 
+		} 
+		else { 
+			
+			// If the length of two arrays is 
+			// different then it will assign 
+			// false into boolean variable 
+			result = false; 
+		} 
+		
+		// After completion to check whether 
+		// result is true of false 
+		if (result == true) { 
+			
+			// Print the result 
+			System.out.println("Arrays are equal"); 
+		} 
+		else { 
+			
+			// Print the result 
+			System.out.println("Arrays are not equal"); 
+		} 
+	} 
+}
